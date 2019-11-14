c1abf5924d21f5561efb47efbfb4acadaccbf201
Jeff Reiner <theundeadghost@gmail.com>
2019-11-14 11:28:35 +0100

    :poop: Trying to fix reloading

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createContainer } from "unstated-next";
 import { ActionSheetIOS, Alert } from "react-native";
 import store from "react-native-simple-store";
 
-import { loadConfig } from "../helpers";
+import { loadConfig, currencies } from "../helpers";
 
 const gasEndpoint = `https://ethereum-api.xyz/gas-prices`;
 const ethEndpoint = `https://ethereum-api.xyz/eth-prices`;
 const guzzlersEndpoint = `https://ethereum-api.xyz/gas-guzzlers`;
+
+const currencyOptionArray = ["USD", "GBP", "EUR", "CAD", "CNY"];
 
 const useApp = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -22,98 +24,123 @@ const useApp = () => {
   const [showGasInCurrency, toggleShowGasInCurrency] = useState(false);
   const [nativeCurrency, setNativeCurrency] = useState("USD");
 
-  const handleShowGasInCurrency = () => {
+  useEffect(() => {
+    const boot = async () => {
+      console.log("App Booted");
+
+      await restoreUserConfig();
+
+      await fetchData();
+
+      setIsLoading(false);
+    };
+
+    boot();
+  }, []);
+
+  const handleShowGasInCurrency = async () => {
     toggleShowGasInCurrency(!showGasInCurrency);
 
-    store.update("config", {
+    await store.update("config", {
       showGasInCurrency: !showGasInCurrency
     });
   };
 
-  const restoreUserConfig = () =>
-    store.get("config").then(config => {
+  const restoreUserConfig = async () => {
+    try {
+      const config = await store.get("config");
+
+      console.log("Config", JSON.stringify(config, null, 2));
+
       if (loadConfig(config)) {
-        toggleShowGasInCurrency(config.showGasInCurrency || showGasInCurrency);
-        setNativeCurrency(config.nativeCurrency || nativeCurrency);
+        toggleShowGasInCurrency(
+          (await config.showGasInCurrency) || showGasInCurrency
+        );
+
+        setNativeCurrency((await config.nativeCurrency) || nativeCurrency);
       }
-
-      return;
-    });
-
-  const handleHardRefresh = () => {
-    setIsLoading(true);
-
-    fetchData().then(() => setIsLoading(false));
+    } catch (err) {
+      console.error(err);
+    }
   };
 
-  const handleRefresh = () => {
+  const handleHardRefresh = async () => {
+    setIsLoading(true);
+
+    await fetchData();
+
+    setIsLoading(false);
+  };
+
+  const handleRefresh = async () => {
     setIsRefreshing(true);
 
-    fetchData().then(() => setIsRefreshing(false));
+    await fetchData();
+
+    setIsRefreshing(false);
+  };
+
+  const handleCurrencyActionSheetBehavior = async buttonIndex => {
+    const selectedCurrency = currencyOptionArray[buttonIndex - 1];
+
+    setNativeCurrency(selectedCurrency);
+
+    await store.update("config", {
+      nativeCurrency: selectedCurrency
+    });
+
+    handleHardRefresh();
   };
 
   const handleChangeCurrency = () => {
-    const currencyOptionArray = ["USD", "GBP", "EUR", "CAD", "CNY"];
-
     ActionSheetIOS.showActionSheetWithOptions(
       {
         options: ["Cancel", ...currencyOptionArray],
         cancelButtonIndex: 0
       },
-      buttonIndex => {
-        if (buttonIndex > 0) {
-          const selectedCurrency = currencyOptionArray[buttonIndex - 1];
-
-          setNativeCurrency(selectedCurrency);
-
-          store.update("config", { nativeCurrency: selectedCurrency });
-
-          handleHardRefresh();
-        }
-      }
+      buttonIndex =>
+        buttonIndex > 0 && handleCurrencyActionSheetBehavior(buttonIndex)
     );
   };
 
-  const handleError = () => {
+  const handleError = () =>
     Alert.alert(
       "Unable To Load Data",
       "Ethereum and gas data can not be loaded at this time.",
       [
         {
           text: "Reload",
-          onPress: () => {
+          onPress: async () => {
             setIsLoading(true);
             setHasErrored(false);
 
-            fetchData().then(() => setIsLoading(false));
+            await fetchData();
+
+            setIsLoading(false);
           }
         }
       ]
     );
-  };
 
   const fetchData = async () => {
+    console.log(nativeCurrency, "nativeCurrency");
+    console.log(showGasInCurrency, "showGasInCurrency");
+
     if (isConnected) {
       try {
         const gasResponse = await fetch(gasEndpoint);
         const gasResponseJson = await gasResponse.json();
-        setGasData(gasResponseJson.result);
+        setGasData(await gasResponseJson.result);
 
         const ethPriceResponse = await fetch(
           `${ethEndpoint}?fiat=${nativeCurrency}`
         );
         const ethPriceResponseJson = await ethPriceResponse.json();
-        setEthData(ethPriceResponseJson.result);
+        setEthData(await ethPriceResponseJson.result);
 
         const guzzlerResponse = await fetch(guzzlersEndpoint);
         const guzzlerResponseJson = await guzzlerResponse.json();
-        setGuzzlerData(guzzlerResponseJson.result);
-
-        return await store.update("cache", {
-          gasData,
-          ethData,
-          guzzlerData
-        });
+        setGuzzlerData(await guzzlerResponseJson.result);
       } catch (error) {
         setHasErrored(true);
         handleError();
@@ -121,21 +148,6 @@ const useApp = () => {
     }
 
     return Promise.resolve();
-  };
-
-  const restoreLastRefreshFromCache = () => {
-    // store.get("cache").then(cache => {
-    //   if (loadConfig(cache)) {
-    //     setGasData(cache.gasData || gasData);
-    //     setEthData(cache.ethData || ethData);
-    //     setGuzzlerData(cache.guzzlerData || guzzlerData);
-
-    //     setIsLoading(false);
-    //   } else {
-    //     fetchData().then(() => setIsLoading(false));
-    //   }
-    // });
-    fetchData().then(() => setIsLoading(false));
   };
 
   return {
@@ -159,7 +171,6 @@ const useApp = () => {
     handleChangeCurrency,
     handleRefresh,
     fetchData,
-    restoreLastRefreshFromCache,
 
     showGasInCurrency,
     toggleShowGasInCurrency,
