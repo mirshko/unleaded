b1430c011c6a39f78fc177b94abdf76af37a2aec
Jeff Reiner <theundeadghost@gmail.com>
2019-11-16 14:37:36 +0100

    :sparkles: Set SafeAreaView background to white

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { SafeAreaView, StatusBar } from "react-native";
 
 export const Container = ({ children }) => (
-  <SafeAreaView style={{ flex: 1 }}>
+  <SafeAreaView style={{ backgroundColor: "white", flex: 1 }}>
     <StatusBar barStyle="dark-content" />
     {children}
   </SafeAreaView>
