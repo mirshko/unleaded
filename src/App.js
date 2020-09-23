35ee23d9f93a2cdc4fcfdd546fdbc0b2a236d0d9
Jeff Reiner <gone@hey.com>
2020-09-23 15:33:05 +0200

    Remove iPad support

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { ActivityIndicator, View } from "react-native";
 import Caps from "./components/Caps";
 import Container from "./components/Container";
 import Divider from "./components/Divider";
-import EthereumPrice from "./components/EthereumPrice";
 import GasSpeed from "./components/GasSpeed";
 import Gutter from "./components/Gutter";
 import Guzzler from "./components/Guzzler";
@@ -14,32 +13,17 @@ import Title from "./components/Title";
 import constants from "./constants";
 import { AppContainer } from "./containers";
 
-const Wrapper = ({ children }) => (
-  <View
-    style={{
-      maxWidth: 512,
-      alignSelf: "center",
-      width: "100%",
-      height: "100%",
-    }}
-  >
-    {children}
-  </View>
-);
-
 const App = () => {
   const data = AppContainer.useContainer();
 
   if (data.hasErrored || data.isLoading)
     return (
       <Container>
-        <Wrapper>
-          <Header />
+        <Header />
 
-          <Pane style={{ marginBottom: constants.headerOffset }}>
-            {data.isLoading && <ActivityIndicator size="large" />}
-          </Pane>
-        </Wrapper>
+        <Pane style={{ marginBottom: constants.headerOffset }}>
+          {data.isLoading && <ActivityIndicator size="large" />}
+        </Pane>
       </Container>
     );
 
@@ -67,66 +51,61 @@ const App = () => {
 
   return (
     <Container>
-      <Wrapper>
-        <Header />
+      <Header />
 
-        <Divider />
+      <Divider />
 
-        <RefreshSwiper
-          refreshFunc={() => data.handleRefresh()}
-          refreshingState={data.refreshing}
-        >
-          <Gutter>
-            <Pane
-              flex={0}
-              alignItems="unset"
-              justifyContent="unset"
-              style={{
-                marginBottom: constants.spacing.xlarge,
-                marginTop: constants.spacing.xlarge,
-              }}
-            >
-              <Title>Gas Speeds</Title>
-              <Caps>By Cost</Caps>
-            </Pane>
-          </Gutter>
+      <RefreshSwiper
+        refreshFunc={() => data.handleRefresh()}
+        refreshingState={data.refreshing}
+      >
+        <Gutter>
+          <Pane
+            flex={0}
+            alignItems="unset"
+            justifyContent="unset"
+            style={{
+              marginBottom: constants.spacing.xlarge,
+              marginTop: constants.spacing.xlarge,
+            }}
+          >
+            <Title>Gas Speeds</Title>
+            <Caps>By Cost</Caps>
+          </Pane>
+        </Gutter>
 
-          <Gutter>
-            {gasSpeeds.map((item, index) => (
-              <React.Fragment key={index}>
-                <GasSpeed
-                  {...item}
-                  style={{
-                    marginBottom:
-                      index !== gasSpeeds.length - 1 && constants.spacing.large,
-                  }}
-                />
-                {index !== gasSpeeds.length - 1 && (
-                  <Divider mb={constants.spacing.large} />
-                )}
-              </React.Fragment>
-            ))}
-          </Gutter>
+        <Gutter>
+          {gasSpeeds.map((item, index) => (
+            <React.Fragment key={index}>
+              <GasSpeed
+                {...item}
+                style={{
+                  marginBottom:
+                    index !== gasSpeeds.length - 1 && constants.spacing.large,
+                }}
+              />
+              {index !== gasSpeeds.length - 1 && (
+                <Divider mb={constants.spacing.large} />
+              )}
+            </React.Fragment>
+          ))}
+        </Gutter>
 
-          <Divider
-            mb={constants.spacing.xlarge}
-            mt={constants.spacing.xlarge}
-          />
+        <Divider mb={constants.spacing.xlarge} mt={constants.spacing.xlarge} />
 
-          <Gutter>
-            <View style={{ marginBottom: constants.spacing.xlarge }}>
-              <Title>Gas Guzzlers</Title>
-              <Caps>By Percent</Caps>
-            </View>
-          </Gutter>
+        <Gutter>
+          <View style={{ marginBottom: constants.spacing.xlarge }}>
+            <Title>Gas Guzzlers</Title>
+            <Caps>By Percent</Caps>
+          </View>
+        </Gutter>
 
-          <Gutter>
-            {data.guzzlerData.slice(0, 10).map((guzzler, index) => (
-              <Guzzler key={index} {...guzzler} />
-            ))}
-          </Gutter>
-        </RefreshSwiper>
-      </Wrapper>
+        <Gutter>
+          {data.guzzlerData.slice(0, 10).map((guzzler, index) => (
+            <Guzzler key={index} {...guzzler} />
+          ))}
+        </Gutter>
+      </RefreshSwiper>
     </Container>
   );
 };
